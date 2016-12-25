aa8a048f01f0a4762fd3f2777b7a43b435bd3065
Ayanfe <ibitoyeayanfeoluwa@yahoo.com>
2016-12-25 21:49:31 +0100

    Add number of admins validation to tier

--- app/models/tier.rb
+++ app/models/tier.rb
@@ -1,3 +1,19 @@
 class Tier < ApplicationRecord
-  validates :admins, numericality: { only_integer: true, even: true }
+  validate :validate_number_of_admins
+
+  def validate_number_of_admins
+    if admins.present?
+      if Tier.count == 0
+        unless admins == 1
+          errors.add(:admins, "must be one")
+        end
+      else
+        unless (admins % Tier.last.admins) == 0
+          errors.add(:admins, "must be divisible by #{Tier.last.admins}")
+        end
+      end
+    else
+      errors.add(:admins, "must be present")
+    end
+  end
 end
